9c948db858c9df7472e4ff5d9bf101b32300cca2
Fardan Fadhilah Andicha Putra <fardan24003@mail.unpad.ac.id>
2025-01-10 10:35:33 +0700

    pause menu update

--- NyamukSimulator/Assets/Script/PauseMenu.cs
+++ NyamukSimulator/Assets/Script/PauseMenu.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
-    public static bool isPaused;
+    public static bool isPaused; // pause status
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        pauseMenu.SetActive(false);
+        pauseMenu.SetActive(false); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape)) // press "Escape" to open pause menu
         {
             if(isPaused)
             {
@@ -27,14 +27,15 @@ public class PauseMenu : MonoBehaviour
         }
     }
     
-    public void pauseGame()
+    public void pauseGame() // pause the game
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
-    public void resumeGame(){
+    public void resumeGame() // resume the game
+    {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
